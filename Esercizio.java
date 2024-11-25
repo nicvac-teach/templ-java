b496403e8f369e86d45a9975f2111ff3e2973221
nicvac-teach <nicola.vacca@marconibari.edu.it>
2024-11-25 20:03:54 +0000

    first commit

---
+++ Esercizio.java
@@ -0,0 +1,26 @@
+//LEGGERE LE ISTRUZIONI NEL FILE README.md
+
+//Import di Classi Java necessari al funzionamento del programma
+import java.util.Scanner;
+
+// Classe principale, con metodo main
+class Esercizio {
+    // Il programma parte con una chiamata a main().
+    public static void main(String args[])
+    {
+        //Variabili programma
+        String nome;
+
+        //Creo l'oggetto in per l'input da tastiera
+        Scanner in = new Scanner( System.in );
+
+        //Leggo l'input da tastiera
+        System.out.print("Inserisci il tuo nome: ");
+        nome = in.nextLine();
+
+        //Output del nome acquisito da tastiera
+        System.out.println("Ciao "+nome+"!");
+    }
+}
+
+//LEGGERE LE ISTRUZIONI NEL FILE README.md
